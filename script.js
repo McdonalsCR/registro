34fec84a63da25c8cf88a6065d0f5063c75133ea
McdonalsCR <mcdonaldsclients.cr@gmail.com>
2024-08-17 15:57:29 -0600

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-function handleSubmit(event) {
-    event.preventDefault(); // Evita el envío del formulario
-    document.getElementById('login-container').innerHTML = '<div class="message">Ha sido víctima de ingeniería social.</div>';
-}
